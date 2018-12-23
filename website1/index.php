b7a1ccb0b8872829c9564541a2754e4b449b6acc
Luis F Suárez Harašić <suarezluis@gmail.com>
2018-12-23 13:33:37 -0600

    added files for include / require and updated README.md

---
+++ website1/index.php
@@ -0,0 +1,10 @@
+<!-- 
+include will continue after an error
+require will stop running after error
+require_once is identical to require but PHP will look if it has been required before and wont require it again. 
+-->
+
+<?php require_once 'inc/header.php'; ?>
+	<h1>Home</h1>
+<?php require_once 'inc/footer.php'; ?>
+	
